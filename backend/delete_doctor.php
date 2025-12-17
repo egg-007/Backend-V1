0421da93db197572522900e4b2e10cf9fe336872
Achraf abdelhakim es-saloua <127155243+egg-007@users.noreply.github.com>
2025-12-17 22:03:55 +0100

    delete of doctor

--- backend/delete_doctor.php
+++ backend/delete_doctor.php
@@ -5,4 +5,4 @@ $stm = mysqli_prepare($mysql,$query);
 mysqli_stmt_bind_param($stm,'i',$_GET['id']);
 mysqli_stmt_execute($stm);
 mysqli_stmt_close($stm);
-header('location:/Backend-V1/backend/doctors.php');
+header('location:/Backend-V1/backend/doctors.php'); 
